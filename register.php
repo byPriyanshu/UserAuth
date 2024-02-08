b971e412b7cb32b506532a729454c1bd6bb80d4c
byPriyanshu <priyanshukatuwal@gmail.com>
2024-02-08 07:44:25 +0545

    deleted register.css link

--- register.php
+++ register.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="styles/index.css">
-    <link rel="stylesheet" href="styles/register.css">
 </head>
 <body>
     <div class="wrapper bg-image">
